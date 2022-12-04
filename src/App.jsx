f1f9825fd61052e07bc715da2630d9ca87f2a7bd
michaelgroves90 <michaeliangroves@gmail.com>
2022-12-04 22:35:06 +0000

    Testimonials and App changes

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Examples from './components/examples/Examples'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
+function App() {
+  return (
+
+    <>
+
+      <Header />
+      <Nav />
+      <About />
+      <Examples />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    
+    </>
+
+  );
+}
+
+export default App;
