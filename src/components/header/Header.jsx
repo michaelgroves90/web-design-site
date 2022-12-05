aa0c9622ce8e3167f529bda38540272f4c8ff0b5
michaelgroves90 <michaeliangroves@gmail.com>
2022-12-05 12:18:44 +0000

    HeaderSocials and draft image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.jpeg'
+import ME from '../../assets/me-transparent-bg.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -19,7 +19,7 @@ const Header = () => {
           <img src={ME} alt="me" />
         </div>
 
-        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
 
       </div>
     </header>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,10 +4,9 @@ import {BsGithub} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
+    <div className='header__socials'>
       <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
       <a href="https://github.com" target="_blank"><BsGithub/></a>
-      <a href="https://linkedin.com" target="_blank"></a>
       </div>
   )
 }
